b815d01e8d54795bc76c5c7ef535aa2b0ae435dc
Toni Spets <toni.spets@iki.fi>
2012-02-14 17:11:58 +0200

    3.03p3 Alpha 1, completely untested

---
+++ Makefile
@@ -0,0 +1,24 @@
+all: ra95
+
+ra95: ra95.exe code.bin tools/patch
+	./tools/patch ra95.exe 2375680 code.bin
+
+code.bin: src/main.asm src/tools.asm src/arguments.asm src/nocd.asm src/max_units_bug.asm src/fence_bug.asm src/tags_bug.asm
+	nasm -isrc/ -f bin -o code.bin src/main.asm
+
+ra95.exe: ra95.dat tools/modpe
+	cp ra95.dat ra95.exe
+	./tools/modpe ra95.exe 1024
+
+ra95.dat:
+	@echo "You are missing the required ra95.dat from 3.03 patch"
+	@false
+
+tools/patch: tools/patch.c
+	gcc -o tools/patch tools/patch.c
+
+tools/modpe: tools/modpe.c tools/modpe.h
+	gcc -o tools/modpe tools/modpe.c
+
+clean:
+	rm -f code.bin tools/modpe tools/patch ra95.exe
---
+++ src/main.asm
@@ -0,0 +1,39 @@
+;
+; Copyright (c) 2012 Toni Spets <toni.spets@iki.fi>
+;
+; Permission to use, copy, modify, and distribute this software for any
+; purpose with or without fee is hereby granted, provided that the above
+; copyright notice and this permission notice appear in all copies.
+;
+; THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+; WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+; MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+; ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+; WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+; ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+; OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+;
+
+[org 0x00710A00]
+[bits 32]
+
+PUSHA
+
+MOV DWORD [0x00589976],version
+
+%include "tools.asm"
+
+; bug fixes
+%include "max_units_bug.asm"
+%include "fence_bug.asm"
+%include "tags_bug.asm"
+
+; extra
+%include "arguments.asm"
+%include "nocd.asm"
+
+POPA
+
+JMP 0x005D6148 ; real module entry point
+
+version: db "3.03p3 A1 ",0
